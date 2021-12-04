9cff1029ab684c6d1d70c4dc2bc7d2ff63d46347
strugglebak <strugglebak@gmail.com>
2021-12-04 19:57:17 +0800

    feat: init repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+use std::io::stdin;
+use std::error::Error;
+
+pub struct InputStringBuffer {
+    pub buffer: String,
+    pub input_string_length: isize,
+}
+
+impl InputStringBuffer {
+    fn new() -> Self {
+        Self {
+            buffer: String::new(),
+            input_string_length: 0,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_string_buffer = InputStringBuffer::new();
+
+    loop {
+        print_prompt();
+        match read_from_stdin() {
+            Ok((line, bytes_read)) => {
+                input_string_buffer.buffer = line.to_string();
+                input_string_buffer.input_string_length = (bytes_read - 1) as isize;
+            },
+            Err(error) => panic!("Problem reading from stdin: {:?}", error),
+        }
+
+        match input_string_buffer.buffer {
+            _ if
+                input_string_buffer.buffer == ".exit" ||
+                input_string_buffer.buffer == ".quit" => {
+                    break;
+                },
+            _ => println!("Unrecognized command '{}'.\n", input_string_buffer.buffer),
+        }
+    }
+}
+
+fn print_prompt() {
+    println!("db > ");
+}
+
+fn read_from_stdin() -> Result<(String, usize), Box<dyn Error>> {
+    let mut line = String::new();
+    let bytes_read = stdin().read_line(&mut line)?;
+
+    // ignore '\r\n'
+    line.pop();
+
+    Ok((line, bytes_read))
 }
