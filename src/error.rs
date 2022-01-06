3d2dbb745b97ad23a5f8282c983f7feafafafe02
strugglebak <strugglebak@gmail.com>
2022-01-06 20:24:09 +0800

    fix: add rstest and pretty assertions for error propmt

--- src/error.rs
+++ src/error.rs
@@ -2,7 +2,6 @@ use std::result;
 use thiserror::Error;
 use sqlparser::parser::ParserError;
 
-
 // 自定义 error
 #[derive(Error, Debug, PartialEq)]
 pub enum NollaDBError {
@@ -22,4 +21,20 @@ pub type Result<T> = result::Result<T, NollaDBError>;
 
 pub fn error_with_general(text: &str) -> NollaDBError {
   NollaDBError::General(text.to_owned())
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use rstest::rstest;
+  use pretty_assertions::assert_eq;
+
+  #[rstest]
+  #[case("test error")]
+  fn general_error_test(#[case] input: &str) {
+    let expected = NollaDBError::General(input.to_string());
+    let result = error_with_general(&input);
+
+    assert_eq!(result, expected);
+  }
 }
