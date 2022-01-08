5c4820e7817529adcbe425ae21a3d5dc6cb039b2
strugglebak <strugglebak@gmail.com>
2022-01-08 19:29:42 +0800

    feat: implement SQL command state and new function

--- src/sql_command/mod.rs
+++ src/sql_command/mod.rs
@@ -1,10 +1,25 @@
 #[derive(Debug, PartialEq)]
 pub enum SQLCommand {
-  // TODO:
+  CreateTable(String),
+  Select(String),
+  Insert(String),
+  Update(String),
+  Delete(String),
+  Unknown(String),
 }
 
 impl SQLCommand {
   pub fn new(command: String) -> SQLCommand {
-    // TODO:
+    let args: Vec<&str> = command.split_whitespace().collect();
+    let first_cmd = args[0].to_owned();
+    // as_ref 将 String 转变成 &str
+    match first_cmd.as_ref() {
+      "create" => SQLCommand::CreateTable(command),
+      "select" => SQLCommand::Select(command),
+      "insert" => SQLCommand::Insert(command),
+      "update" => SQLCommand::Update(command),
+      "delete" => SQLCommand::Delete(command),
+      _ => SQLCommand::Unknown(command),
+    }
   }
 }
