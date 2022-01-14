eb4e6e6d5934d2745e81c14657512a553ee5ce9d
strugglebak <strugglebak@gmail.com>
2022-01-14 14:53:59 +0800

    feat: add table row

--- src/table/mod.rs
+++ src/table/mod.rs
@@ -1,1 +1,14 @@
 mod datatype;
+mod row;
+
+use std::collections::{HashMap};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, PartialEq, Debug)]
+pub struct Table {
+  pub table_name: String,
+  pub indexes: HashMapM<String, String>,
+  pub most_recent_row_id: i64,
+  pub primary_key: String,
+}
---
+++ src/table/row.rs
@@ -0,0 +1,38 @@
+use std::collections::{BTreeMap};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, PartialEq, Debug)]
+pub enum Row {
+  Integer(BTreeMap<i64, i32>),
+  Bool(BTreeMap<i64, bool>),
+  Text(BTreeMap<i64, String>),
+  Real(BTreeMap<i64, f32>),
+  None,
+}
+
+// 这里是通过 Row 构建 Column
+// 所以这里可以通过 Row 来推断出 Column 的情况
+// Column 有一个 Index，这个 Index 也是由 BTreeMap 管理
+// 这个 Index 里的 BTreeMap 存的 key/value 跟 Row 里面的 key/value 刚好相反
+impl Row {
+  fn get_number_of_element_in_column(&self) -> usize {
+    match self {
+      Row::Integer(tree) => tree.len(),
+      Row::Bool(tree) => tree.len(),
+      Row::Text(tree) => tree.len(),
+      Row::Real(tree) => tree.len(),
+      Row::None => panic!("Found None Type in columns"),
+    }
+  }
+
+  fn get_serialized_column_data(&self) -> Vec<String> {
+    match self {
+      Row::Integer(tree) => tree.iter().map(|(_key, value)| value.to_string*()).collect(),
+      Row::Bool(tree) => tree.iter().map(|(_key, value)| value.to_string*()).collect(),
+      Row::Text(tree) => tree.iter().map(|(_key, value)| value.to_string*()).collect(),
+      Row::Real(tree) => tree.iter().map(|(_key, value)| value.to_string*()).collect(),
+      Row::None => panic!("Found None Type in columns"),
+    }
+  }
+}
