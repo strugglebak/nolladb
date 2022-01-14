451667e8d6a3b417c7213d15a42c80d1b411479d
strugglebak <strugglebak@gmail.com>
2022-01-14 15:30:44 +0800

    feat: add data_type and column

---
+++ src/table/column.rs
@@ -0,0 +1,54 @@
+mod index;
+mod data_type;
+
+use std::collections::{BTreeMap};
+
+use serde::{Deserialize, Serialize};
+
+use index::Index;
+use data_type::DataType;
+
+#[derive(Deserialize, Serialize, PartialEq, Debug)]
+pub struct Column {
+  pub column_name: String,
+  pub column_datatype: DataType,
+  pub is_primary_key: bool,
+  pub is_unique_constraint: bool,
+  pub is_not_null_constraint: bool,
+  pub is_indexed: bool,
+  pub index: Index;
+}
+
+impl Column {
+  pub fn new(
+    column_name: String,
+    column_datatype: String,
+    is_primary_key: bool,
+    is_unique_constraint: bool,
+    is_not_null_constraint: bool,
+  ) -> Self {
+    let cd = DataType::new(column_datatype);
+    let index = match column_datatype {
+      DataType::Integer => Index::Integer(BTreeMap::new()),
+      DataType::Bool => Index::None,
+      DataType::Text => Index::Text(BTreeMap::new()),
+      DataType::Real => Index::None,
+      DataType::None => Index::None,
+      DataType::Invalid => Index::None,
+    };
+
+    Column {
+      column_name,
+      column_datatype: cd,
+      is_primary_key,
+      is_unique_constraint,
+      is_not_null_constraint,
+      is_indexed: is_primary_key,
+      index,
+    }
+  }
+
+  pub fn get_mut_index(&mut self) -> &mut Index {
+    &mut self.index
+  }
+}
---
+++ src/table/data_type.rs
@@ -0,0 +1,40 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, PartialEq, Debug)]
+pub enum DataType {
+  Integer,
+  Bool,
+  Text,
+  Real,
+  None,
+  Invalid,
+}
+
+impl DataType {
+  pub fn new(command: String) -> DataType {
+    match command.to_lowercase().as_ref() {
+      "integer" => DataType::Integer,
+      "bool" => DataType::Bool,
+      "text" => DataType::Text,
+      "real" => DataType::Real,
+      "none" => DataType::None,
+      _ => {
+        eprintln!("Invalid datatype: {}", command)
+        return DataType::Invalid;
+      },
+    }
+  }
+}
+
+impl fmt::Display for DataType {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match *self {
+      DataType::Integer => f.write_str("Integer"),
+      DataType::Bool => f.write_str("Boolean"),
+      DataType::Text => f.write_str("Text"),
+      DataType::Real => f.write_str("Real"),
+      DataType::None => f.write_str("None"),
+      DataType::Invalid => f.write_str("Invalid"),
+    }
+  }
+}
