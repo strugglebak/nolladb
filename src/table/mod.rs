11fe3763eb38165c8af23e9999c39efe8dfd514f
strugglebak <strugglebak@gmail.com>
2022-01-14 14:57:44 +0800

    feat: use Row in table

--- src/table/mod.rs
+++ src/table/mod.rs
@@ -2,13 +2,18 @@ mod datatype;
 mod row;
 
 use std::collections::{HashMap};
+use std::rc::Rc;
+use std::cell::RefCell;
 
 use serde::{Deserialize, Serialize};
 
+use row::Row;
+
 #[derive(Deserialize, Serialize, PartialEq, Debug)]
 pub struct Table {
+  pub primary_key: String,
   pub table_name: String,
   pub indexes: HashMapM<String, String>,
   pub most_recent_row_id: i64,
-  pub primary_key: String,
+  pub rows: Rc<RefCell<HashMap<String, Row>>>,
 }
