c2c5d2183a6eb0c81e636a0e33ce915c8c7bbb1f
strugglebak <strugglebak@gmail.com>
2022-01-17 23:39:53 +0800

    feat: implement insert_row in table

--- src/table/mod.rs
+++ src/table/mod.rs
@@ -146,7 +146,7 @@ impl Table {
     Err(NollaDBError::General(String::from("Column not found.")))
   }
 
-  pub fn get_column_mut<'a>(&mut self, column_name: String) -> Result<&mut Column> {
+  pub fn get_column_mut(&mut self, column_name: String) -> Result<&mut Column> {
     // TODO: 待优化
     for table_column in self.table_columns.iter_mut() {
       if table_column.column_name == column_name {
@@ -163,10 +163,7 @@ impl Table {
     table_column_names: &Vec<String>,
     table_column_value: &Vec<String>,
   ) -> Result<()> {
-    for (i, table_column_name) in
-      table_column_names
-      .iter()
-      .enumerate() {
+    for (i, table_column_name) in table_column_names.iter().enumerate() {
       let table_column = self.get_column_mut(table_column_name.to_string()).unwrap();
       let Column { index, column_name, .. } = &table_column;
 
@@ -174,14 +171,12 @@ impl Table {
       // 或者
       // 如果当前插入的 column name 和表中的 column name 不一致也继续找
       if !table_column.is_unique_constraint ||
-         *table_column_name != *column_name {
-        continue;
-      }
+         *table_column_name != *column_name { continue; }
 
       let column_value = &table_column_value[i];
       match index {
         Index::Integer(tree) => {
-          if tree.contains_key(&column_value.parse::<i64>().unwrap()) {
+          if tree.contains_key(&column_value.parse::<i32>().unwrap()) {
             return Err(
               NollaDBError::General(
                 format!(
@@ -194,7 +189,7 @@ impl Table {
           }
         },
         Index::Text(tree) => {
-          if tree.contains_key(&column_value.parse::<i64>().unwrap()) {
+          if tree.contains_key(column_value) {
             return Err(
               NollaDBError::General(
                 format!(
@@ -227,6 +222,131 @@ impl Table {
     table_column_names: &Vec<String>,
     table_column_value: &Vec<String>,
   ) {
+    let mut new_row_id = self.most_recent_row_id + i64::from(1);
 
+    let table_rows_clone = Rc::clone(&self.table_rows);
+    let mut table_rows_data =
+      table_rows_clone
+        .as_ref()
+        .borrow_mut();
+    let mut table_certain_column_data =
+      table_rows_data
+        .get_mut(&self.primary_key)
+        .unwrap();
+
+    match self.primary_key == "-1" {
+      // 没设置 PRIMARY KEY
+      true => {
+        if let Row::Integer(_) = &mut table_certain_column_data {
+          for (i, table_column_name) in table_column_names.iter().enumerate() {
+            if table_column_name != &self.primary_key { continue; }
+            let value = &table_column_value[i];
+            new_row_id = value.parse::<i64>().unwrap();
+          }
+        }
+      },
+      // 设置了 PRIMARY KEY
+      false => {
+        if !table_column_names
+          .iter()
+          .any(|table_column_name| table_column_name == &self.primary_key) {
+
+          if let Row::Integer(row_tree) = &mut table_certain_column_data {
+            let key = new_row_id.clone();
+            let value = new_row_id as i32;
+
+            row_tree.insert(key, value);
+
+            let table_certain_column_index =
+              self
+                .get_column_mut(self.primary_key.to_string())
+                .unwrap()
+                .get_index_mut();
+            if let Index::Integer(column_tree) = table_certain_column_index {
+              column_tree.insert(value, key);
+            }
+
+          }
+
+        }
+
+      },
+    }
+
+    // 检查 INSERT statement 中是否有表中没有的 column
+    // 如果是，就对该 column 补 "Null"
+    // 使得 row 的长度能对齐
+    let mut j: usize = 0;
+    let column_names_vec = self
+        .table_columns
+        .iter()
+        .map(|table_column| table_column.column_name.to_string())
+        .collect::<Vec<String>>();
+
+    for i in 0..column_names_vec.len() {
+      let key = &column_names_vec[i];
+      let mut value = String::from("Null");
+
+      match &table_column_names.get(j) {
+        Some(table_column_name) => {
+          if key == &table_column_name.to_string() {
+            value = table_column_value[j].to_string();
+            j += 1;
+          } else {
+            // 如果在原来的表中对应 column 名字不匹配
+            // 如果是 PRIMARY KEY 则跳过
+            if key == &self.primary_key { continue; }
+          }
+        },
+        _ => {
+          // 找到末尾
+          // 如果是 PRIMARY KEY 则跳过
+          if key == &self.primary_key { continue; }
+        },
+      }
+
+      // 拿到这个 key 对应的 column data
+      let mut table_key_corresponding_column_data =
+        table_rows_data
+          .get_mut(key)
+          .unwrap();
+
+      // 拿到这个 key 对应的 column index
+      let table_key_corresponding_column_index =
+        self
+          .get_column_mut(key.to_string())
+          .unwrap()
+          .get_index_mut();
+
+
+      // 更新
+      let key = new_row_id.clone();
+      match &mut table_key_corresponding_column_data {
+        Row::Integer(row_tree) => {
+          let value = value.parse::<i32>().unwrap();
+          row_tree.insert(key, value);
+          if let Index::Integer(column_tree) = table_key_corresponding_column_index {
+            column_tree.insert(value, key);
+          }
+        },
+        Row::Bool(row_tree) => {
+          let value = value.parse::<bool>().unwrap();
+          row_tree.insert(key, value);
+        },
+        Row::Text(row_tree) => {
+          row_tree.insert(key, value.to_string());
+          if let Index::Text(column_tree) = table_key_corresponding_column_index {
+            column_tree.insert(value.to_string(), key);
+          }
+        },
+        Row::Real(row_tree) => {
+          let value = value.parse::<f32>().unwrap();
+          row_tree.insert(key, value);
+        },
+        Row::None => panic!("None data Found"),
+      }
+    }
+
+    self.most_recent_row_id = new_row_id;
   }
 }
